c86feaa7efa32a4961433add52f3f1ac30aac6ce
Yash Goyal <yashgoyal.ec@gmail.com>
2026-08-17 03:24:13 +0530

    Implement node style api call for vercel

--- frontend/api/leetcode.js
+++ frontend/api/leetcode.js
@@ -1,4 +1,4 @@
-export default async function handler(request){
+export default async function handler(req,res){
     console.log("API function started");
     const query = `
         query userPublicProfile($username : String!){
@@ -57,5 +57,5 @@ export default async function handler(request){
     rating: rating,
     globalRanking: globalRanking
     };
-    return Response.json(leetcodeStats)
+    return res.status(200).json(leetcodeStats)
 }
